0da4eae849fde4abf1e39e6fabbe5b86dcb0b02d
Subhajit Ghosh <subhajitstd07@gmail.com>
2022-11-13 15:52:55 +0530

    [Testimonial.js, TestimonialCard.js and Faq.js]

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,26 +2,26 @@ import React from 'react'
 
 function Faq() {
   return (
-    <div className='bg-teal-300 m-3'>
+    <div className='bg-[#77ffd4] m-3'>
         <section className=" text-gray-800">
 	<div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
 		<h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl drop-shadow-md">Frequently Asked Questions</h2>
 		<div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
 			<div>
-				<h3 className="text-3xl font-normal">Lorem ipsum dolor sit amet.</h3>
-				<p className="mt-1 italic text-stone-900 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+				<h3 className="text-3xl font-medium">Lorem ipsum dolor sit amet.</h3>
+				<p className="mt-1 italic text-stone-900 text-justify font-normal text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
 			</div>
 			<div>
-				<h3 className="text-3xl font-normal">Lorem ipsum dolor sit amet.</h3>
-				<p className="mt-1 italic text-stone-900 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+				<h3 className="text-3xl font-medium">Lorem ipsum dolor sit amet.</h3>
+				<p className="mt-1 italic text-stone-900 text-justify font-normal text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
 			</div>
 			<div>
-				<h3 className="text-3xl font-normal">Lorem ipsum dolor sit amet.</h3>
-				<p className="mt-1 italic text-stone-900 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+				<h3 className="text-3xl font-medium">Lorem ipsum dolor sit amet.</h3>
+				<p className="mt-1 italic text-stone-900 text-justify font-normal text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
 			</div>
 			<div>
-				<h3 className="text-3xl font-normal">Lorem ipsum dolor sit amet.</h3>
-				<p className="mt-1 italic text-stone-900 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+				<h3 className="text-3xl font-medium">Lorem ipsum dolor sit amet.</h3>
+				<p className="mt-1 italic text-stone-900 text-justify font-normal text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
 			</div>
 		</div>
 	</div>
